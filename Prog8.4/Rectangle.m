159cb7ed5a7e12d44f7ade9f9be2ea5bb9493caa
admin <dangtinhtong@gmail.com>
2017-12-01 13:59:23 +0700

    First commit

---
+++ Prog8.4/Rectangle.m
@@ -0,0 +1,38 @@
+//
+//  Rectangle.m
+//  Prog8.4
+//
+//  Created by admin on 12/1/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import "Rectangle.h"
+
+@implementation Rectangle
+@synthesize width,height;
+-(void) setWidth:(int)w andHeight:(int)h
+{
+    width = w;
+    height = h;
+    
+}
+
+-(void) setOrigin:(XYPoint *)pt
+{
+    origin = pt;
+}
+
+-(int) area
+{
+    return width*height;
+}
+
+-(int) perimeter
+{
+    return (width+height)*2;
+}
+-(XYPoint*) origin
+{
+    return origin  ;
+}
+@end
--- Prog8.4/main.m
+++ Prog8.4/main.m
@@ -2,16 +2,36 @@
 //  main.m
 //  Prog8.4
 //
-//  Created by admin on 12/1/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 12/1/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
+#include "XYPoint.h"
+#include "Rectangle.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
+        
+        Rectangle *myRectangle = [[Rectangle alloc]  init];
+        XYPoint *myXYPoint = [[XYPoint alloc] init];
+        // Set x,y =(100,200)
+      //  [myXYPoint setX:100 andY:200];
+        
+        // set width=5 and height =8
+        [myRectangle setWidth:5 andHeight:8];
+        
+        NSLog(@"Rectangle width: %i, height: %i",myRectangle.width,myRectangle.height);
+        
+        NSLog(@"Area = %i, Perimeter = %i",[myRectangle area],[myRectangle perimeter]);
+        
+        [myXYPoint setX:50 andY:50];
+        //NSLog(@"Origin at (%i,%i)", myRectangle.origin.x,myRectangle.origin.y);
+        myRectangle.origin= myXYPoint;
+        NSLog(@"Origin at (%i,%i)",myRectangle.origin.x, myRectangle.origin.y);
+        
     }
     return 0;
 }
